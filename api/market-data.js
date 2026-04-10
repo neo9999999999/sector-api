a715c31e03b7fd62851ec5ec50df7cfcbfe4a1c0
fix <fix@fix.com>
2026-04-10 07:34:43 +0000

    fix: volume-rank SCR=20171, EXLS=0000000000 (공식 파라미터)

--- api/market-data.js
+++ api/market-data.js
@@ -40,10 +40,10 @@ module.exports=async(req,res)=>{
 
     // ① 거래대금 순위 (코스피+코스닥 통합)
     const vp={
-      FID_COND_SCR_DIV_CODE:"20174",FID_INPUT_ISCD:"0000",
+      FID_COND_SCR_DIV_CODE:"20171",FID_INPUT_ISCD:"0000",
       FID_COND_MRKT_DIV_CODE:"J",FID_DIV_CLS_CODE:"0",
       FID_BLNG_CLS_CODE:"0",FID_TRGT_CLS_CODE:"111111111",
-      FID_TRGT_EXLS_CLS_CODE:"000000",FID_INPUT_PRICE_1:"0",
+      FID_TRGT_EXLS_CLS_CODE:"0000000000",FID_INPUT_PRICE_1:"0",
       FID_INPUT_PRICE_2:"1000000",FID_VOL_CNT:"100000",FID_INPUT_DATE_1:""
     };
     const volRes=await get("/uapi/domestic-stock/v1/quotations/volume-rank","FHPST01710000",vp,tk);
